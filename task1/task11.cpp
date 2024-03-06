14c5c16dd69f59f98aed9f8abcaf6bf79ebd9342
123abc1920 <123abc1920@mail.ru>
2024-03-06 19:34:02 +0300

    add task1

--- task1/task11.cpp
+++ task1/task11.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    double n = 1, term = 1.0 / 2.0, sum = 0;
+    double n = 1, term = 0.5, sum = 0;
 
     do
     {
@@ -11,20 +11,20 @@ int main()
         term *= n;
         n++;
         term /= 2.0 * n;
+        // cout << sum << endl;
     } while (term > 1e-6);
 
     cout << "Сумма ряда: " << sum << endl;
 
     /*------------------*/
 
-    n = 1, term = 1.0 / 2.0, sum = 0;
+    n = 1, term = 0.5, sum = 0;
     double a = 0;
 
     do
     {
         sum += term;
-        ((term *= n) /= 2.0) /= (n + 1);
-        n++;
+        (((term *= 0.5) *= n) /= (n + 1), n++);
     } while (term > 1e-6);
 
     cout << "Сумма ряда: " << sum << endl;
